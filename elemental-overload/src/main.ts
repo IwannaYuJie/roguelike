9023563bf10da250bf912632b8bb1f4fcd04e296
C:/Program Files (x86)/Git/ <958656603@qq.com>
2025-09-30 13:43:28 +0800

    feat: 配置Netlify部署与优化HTML入口

--- elemental-overload/src/main.ts
+++ elemental-overload/src/main.ts
@@ -31,7 +31,7 @@ const config: Phaser.Types.Core.GameConfig = {
     default: 'arcade',
     arcade: {
       debug: false,
-      gravity: { y: 0 },
+      gravity: { x: 0, y: 0 },
     },
   },
   scene: [BootScene, MainMenuScene, GameScene, UIScene],
